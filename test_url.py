f2af5ca22daba92ca325959050e0f7f03a4650e2
Jean JAYET <jean.jayet38@gmail.com>
2020-03-23 11:22:23 +0000

    replace usage of urllib3 with urllib to manage URL operations

--- test_url.py
+++ test_url.py
@@ -1,19 +1,24 @@
 import pytest
-import urllib3
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 
 def test_url(url,status):
-   http = urllib3.PoolManager()
+   req = Request(url)
    try:
-    resp = http.request('GET', url)
-   except urllib3.exceptions.HTTPError as e:
+    resp = urlopen(req)
+   except HTTPError as e:
       print('The server couldn\'t fulfill the request')
-      print('Error code: ', e)
+      print('Error code: ', e.code)
+      assert status == "not_found"
+   except URLError as e:
+      print('Failed to reach server')
+      print('Reason: ', e.reason)
       assert status == "not_found"
    else:
       if (status == "found"):
-       assert (resp.status == 200),"URL not reachable"
+       assert (resp.code == 200),"url not reachable"
       elif (status == "not_found"):
-       assert (resp.status != 200),"URL reachable"
+       assert (resp.code != 200),"url reachable"
       else:
-         print("Cannot determine URL status to check")
+         print("Cannot determine url status to check")
          assert True == False
